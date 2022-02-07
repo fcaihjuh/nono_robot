fcc38456a873ccb0b441bbe7093c780dbda31af8
fcaihjuh <florence0cai@gmail.com>
2022-02-07 14:42:59 +0100

    create buttons.js

---
+++ Application/js/buttons.js
@@ -0,0 +1,19 @@
+function playAction() {
+    console.log("Let's start")
+}
+
+function pauseAction() {
+    console.log("I'm tired.")
+}
+
+function restartAction() {
+    console.log('Reconfigure your robot')
+}
+
+function stopAction() {
+    console.log('The robot stopped')
+}
+
+function saveAction() {
+    console.log('your work has been saved !')
+}
